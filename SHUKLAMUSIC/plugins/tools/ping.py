39089aa46b6e3c10a529e82d85adac5ff5676efc
XDMUSIX <147618918+XDMUSIX@users.noreply.github.com>
2024-03-30 09:00:31 +0530

    Add files via upload

--- SHUKLAMUSIC/plugins/tools/ping.py
+++ SHUKLAMUSIC/plugins/tools/ping.py
@@ -1,29 +1,80 @@
 from datetime import datetime
-
 from pyrogram import filters
 from pyrogram.types import Message
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
-from config import *
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from SHUKLAMUSIC import app
 from SHUKLAMUSIC.core.call import SHUKLA
 from SHUKLAMUSIC.utils import bot_sys_stats
 from SHUKLAMUSIC.utils.decorators.language import language
 from SHUKLAMUSIC.utils.inline import supp_markup
-from config import BANNED_USERS
+from config import BANNED_USERS, PING_IMG_URL
+import aiohttp
+import asyncio
+from io import BytesIO
+from SHUKLAMUSIC import app
+from pyrogram import filters
 
+
+
+async def make_carbon(code):
+    url = "https://carbonara.solopov.dev/api/cook"
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json={"code": code}) as resp:
+            image = BytesIO(await resp.read())
+    image.name = "carbon.png"
+    return image
 
 @app.on_message(filters.command("ping", prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
+    PING_IMG_URL = "https://telegra.ph/file/7bb907999ea7156227283.jpg"
+    captionss = "**🥀ᴘɪɴɢɪɴɢ ᴏᴜʀ sᴇʀᴠᴇʀ ᴡᴀɪᴛ.**"
+    response = await message.reply_photo(PING_IMG_URL, caption=(captionss))
+    await asyncio.sleep(1)
+    await response.edit_caption("**🥀ᴘɪɴɢɪɴɢ ᴏᴜʀ sᴇʀᴠᴇʀ ᴡᴀɪᴛ...**")
+    await asyncio.sleep(1)
+    await response.edit_caption("**🥀ᴘɪɴɢɪɴɢ ᴏᴜʀ sᴇʀᴠᴇʀ ᴡᴀɪᴛ.**")
+    await asyncio.sleep(1)
+    await response.edit_caption("**🥀ᴘɪɴɢɪɴɢ ᴏᴜʀ sᴇʀᴠᴇʀ ᴡᴀɪᴛ..**")
+    await asyncio.sleep(1.5)
+    await response.edit_caption("**🥀ᴘɪɴɢɪɴɢ ᴏᴜʀ sᴇʀᴠᴇʀ ᴡᴀɪᴛ...**")
+    await asyncio.sleep(2)
+    await response.edit_caption("**🥀ᴘɪɴɢɪɴɢ ᴏᴜʀ sᴇʀᴠᴇʀ ᴡᴀɪᴛ....**")
+    await asyncio.sleep(2)
+    await response.edit_caption("**📡sʏsᴛᴇᴍ ᴅᴀᴛᴀ ᴀɴᴀʟʏsᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ !**")
+    await asyncio.sleep(3)
+    await response.edit_caption("**📩sᴇɴᴅɪɴɢ sʏsᴛᴇᴍ ᴀɴᴀʟʏsᴇᴅ ᴅᴀᴛᴀ ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ...**")
     start = datetime.now()
-    response = await message.reply_video(
-        video="https://graph.org/file/5690109178f081adf464d.mp4",
-        caption=_["ping_1"].format(app.mention),
-    )
     pytgping = await SHUKLA.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
-    await response.edit_text(
-        _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
-        reply_markup=supp_markup(_),
-    )
+    text =  _["ping_2"].format(resp, app.name, UP, RAM, CPU, DISK, pytgping)
+    carbon = await make_carbon(text)
+    captions = "**ㅤ  🏓 ᴘɪɴɢ...ᴘᴏɴɢ...ᴘɪɴɢ✨\nㅤ  🎸 sʜɪᴠ...sʜɪᴠᴀɴsʜ...ʙᴀʙʏ💞**"
+    await message.reply_photo((carbon), caption=captions,
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+            InlineKeyboardButton(
+                text=_["S_B_5"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        
+        ],
+        [
+            InlineKeyboardButton(
+                text="✦ ɢʀᴏᴜᴘ ✦", url=f"https://t.me/MASTIWITHFRIENDSX",
+            ),
+            InlineKeyboardButton(
+                text="✧ ᴍᴏʀᴇ ✧", url=f"https://t.me/SHIVANSH474",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="❅ ʜᴇʟᴘ ❅", callback_data="settings_back_helper"
+            )
+        ],
+    ]
+    ),
+        )
+    await response.delete()
